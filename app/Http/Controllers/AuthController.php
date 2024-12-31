bfcf790300e5c8a9aa2486fa42dbcc8602ab1bfc
Luan Pereira da Silva <luanpereirahp0@gmail.com>
2024-12-31 00:14:50 -0300

    conectando ao banco de dados

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Container\Attributes;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -13,7 +16,37 @@ class AuthController extends Controller
     }
 
     public function loginSubmit(Request $request){
-        echo 'loginSubmit';
+
+        // form validation 
+        $request->validate(
+            [
+                'text_username' => 'required|email',
+                'text_password' => 'required|min:6|max:12'
+            ],
+             // error messages
+            [
+                'text_username.required' => 'O username é obrigatório',
+                'text_username.email' => 'O username deve ser um email válido',
+                'text_password.required' => 'A senha é obrigatória',
+                'text_password.min' => 'A senha deve ter no mínimo :min caracteres',
+                'text_password.max' => 'A senha deve ter no máximo :max caracteres',
+                
+            ]
+
+        );
+
+        // get user input
+        $username = $request->input('text_username');
+        $password = $request->input('text_password');
+
+        // test database connection 
+        try{
+            DB::connection()->getPdo();
+            echo 'Connected successfully to: ';
+        }catch(\PDOException $e){
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+        echo 'fim';
     }
 
     public function logout(){
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,15 +13,23 @@
                     <!-- form -->
                     <div class="row justify-content-center">
                         <div class="col-md-10 col-12">
-                            <form action="/loginSubmit" method="post">
+                            <form action="/loginSubmit" method="post" novalidate>
                                 @csrf
                                 <div class="mb-3">
                                     <label for="text_username" class="form-label">Username</label>
-                                    <input type="text" class="form-control bg-dark text-info" name="text_username" required>
+                                    <input type="email" class="form-control bg-dark text-info" name="text_username" value="{{ old('text_username') }}" required>
+                                    {{-- show errors --}}
+                                    @error('text_username')
+                                        <div class="text-danger"> {{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <label for="text_password" class="form-label">Password</label>
-                                    <input type="password" class="form-control bg-dark text-info" name="text_password" required>
+                                    <input type="password" class="form-control bg-dark text-info" name="text_password" value="{{ old('text_password') }}" required>
+                                    {{-- show errors --}}
+                                    @error('text_password')
+                                        <div class="text-danger"> {{ $message }}</div>
+                                    @enderror
                                 </div>
                                 <div class="mb-3">
                                     <button type="submit" class="btn btn-secondary w-100">LOGIN</button>
@@ -34,6 +42,8 @@
                     <div class="text-center text-secondary mt-3">
                         <small>&copy; <?= date('Y') ?> Notes</small>
                     </div>
+
+                    
 
                 </div>
             </div>
